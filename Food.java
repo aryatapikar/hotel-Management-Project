ce6e346dd0f8ff4cefae14088d2e48774fe66a6e
aryatapikar <arya.tapikar@gmail.com>
2024-04-19 10:32:34 +0530

    added files to the project

---
+++ Food.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+public class Food implements Serializable 
+{
+    int itemno;
+    int quantity;
+    float price;
+
+    public Food(int itemno, int quantity) {
+        this.itemno = itemno;
+        this.quantity = quantity;
+        calculatePrice();
+    }
+
+    private void calculatePrice() {
+        switch (itemno) {
+            case 1:
+                price = quantity * 50;
+                break;
+            case 2:
+                price = quantity * 60;
+                break;
+            case 3:
+                price = quantity * 70;
+                break;
+            case 4:
+                price = quantity * 30;
+                break;
+            default:
+                // Handle invalid item numbers here
+                break;
+        }
+    }
+}
